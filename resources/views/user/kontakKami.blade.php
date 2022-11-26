f8f2b913a7f7430cca5f6a7aa04d5334f983c1e6
@wira.timothy471 <wira.timothy47@gmail.com>
2022-11-26 14:34:40 +0700

    first

--- resources/views/user/kontakKami.blade.php
+++ resources/views/user/kontakKami.blade.php
@@ -11,10 +11,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
 <body>
-    <h3 style="font-weight: bold">Jl. Pisang Kipas No. 10, Kota Malang</h3>
-    <h3 style="font-weight: bold">0822-2911-4811</h3>
+<h3 style="font-weight: bold">Jl. Pisang Kipas No.10, Jatimulyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65141</h3>
+    <h3 style="font-weight: bold">0822-2911-4811 <a href="https://wa.me/6282229114811" target="_blank">Hubungi kami di Whatsapp <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+        <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+        </svg></a></h3>
     <h3 style="font-weight: bold">Senin - Sabtu 08.00-22.00WIB</h3>
-    <h3><a href="https://goo.gl/maps/7B57yyjBETKjKwsV6" style="bold text-decoration: none">Google Maps Apotek Firdan Farma</a></h3>
+    <h3><a href="https://goo.gl/maps/7B57yyjBETKjKwsV6" target="_blank" style="bold text-decoration: none">Google Maps Apotek Firdan Farma</a></h3>
+<div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.548715538635!2d112.6158277!3d-7.942108400000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7883798236b541%3A0x4cb7709928bc4f6e!2sApotek%20Firdan%20Farma!5e0!3m2!1sid!2sid!4v1669447509550!5m2!1sid!2sid" 
+    width="80%" height="450" style="border:0;" allowfullscreen="" 
+    loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
 </body>
 </html>
 
