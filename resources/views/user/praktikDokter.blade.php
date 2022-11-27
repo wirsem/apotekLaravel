0ed50197fbe95a78458cfe2b6dd159b95c49c426
@wira.timothy471 <wira.timothy47@gmail.com>
2022-11-27 21:54:05 +0700

    first

--- resources/views/user/praktikDokter.blade.php
+++ resources/views/user/praktikDokter.blade.php
@@ -11,7 +11,30 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
 <body>
-    <h3 style="font-weight: bold">BELOM DITAMBAHIN</h3>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">No</th>
+            <th scope="col">Nama Dokter</th>
+            <th scope="col">Hari</th>
+            <th scope="col">Jam Praktik</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Dokter 1</td>
+            <td>Senin - Rabu </td>
+            <td>07.00 - 10.00 WIB</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Dokter 2</td>
+            <td>Kamis - Jum'at</td>
+            <td>07.00 - 10.00 WIB</td>
+          </tr>
+        </tbody>
+      </table>
 </body>
 </html>
 @stop
